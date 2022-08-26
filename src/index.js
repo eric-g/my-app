da6aaef7b1e2a90109f869bfc3ea19f0478eee0f
ericg <gausnell@gmail.com>
2022-08-25 18:34:22 -0700

    index X!

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: X!';
 
         return (
             <div>
